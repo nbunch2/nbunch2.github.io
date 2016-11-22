134120885e8309577f787e5be7dec526108a2c00
nbunch2 <n.bunch5683@gmail.com>
2016-11-21 20:20:15 -0700

    Pop-up Window Worked

---
+++ jsonweather-html/scripts/remote_wu.js
@@ -0,0 +1,47 @@
+// Current Location Scripts
+$(function () {
+
+  var status = $('#status');
+
+  (function getGeoLocation() {
+    status.text('Getting Location...');
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function (position) {
+        var lat = position.coords.latitude;
+        var long = position.coords.longitude;
+
+        // Call the getData function, send the lat and long
+        getData(lat, long);
+
+      });
+    } else {
+      status.text("Your browser doesn't support Geolocation or it is not enabled!");
+    }
+
+  })();
+    
+  // Get the data from the wunderground API
+  function getData(lat, long){
+    $.ajax({
+  url : "http://api.wunderground.com/api/47d4acfe313b2d41/geolookup/conditions/q/" + lat + ',' + long + ".json",
+  dataType : "jsonp",
+  success : function(parsed_json) {
+  var location = parsed_json['location']['city'];
+  var temp_f = parsed_json['current_observation']['temp_f'];
+  alert("Current temperature in " + location + " is: " + temp_f);
+  
+  }
+}); 
+
+
+      $("#cover").fadeOut(250);
+    }
+           });
+
+  
+
+  // A function for changing a string to TitleCase
+  function toTitleCase(str){
+    return str.replace(/\w+/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+  
+};
