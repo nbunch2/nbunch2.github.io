1c5d226aaa527dcefb7450792b43c43757c2760c
nbunch2 <n.bunch5683@gmail.com>
2016-11-28 09:08:27 -0700

    https

---
+++ save/scripts/modules.js
@@ -0,0 +1,8 @@
+//Jquery function to implement modularization
+
+$(function() {
+    $("#page-header").load("/save/modules/header.html");
+    $("#page-nav").load("/save/modules/nav.html");
+    $("#page-footer").load("/save/modules/footer.html");    
+}
+    );
