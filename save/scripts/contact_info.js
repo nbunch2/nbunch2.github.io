55744f729561c841f4973ea86360ecad3e997568
nbunch2 <n.bunch5683@gmail.com>
2016-12-09 20:25:04 -0700

    here we go

--- save/scripts/contact_info.js
+++ save/scripts/contact_info.js
@@ -1,7 +1,7 @@
-$(document).ready(function(){
+$(document).ready(sfunction(){
     $.getJSON( "/save/scripts/contact_info.json", function( data ) {
-  console.log(data)
-    $(".info").text(data.TroyHicks.City)
+  console.log(data);
+    $(".info").text(data.TroyHicks.City);
     
     });
     
