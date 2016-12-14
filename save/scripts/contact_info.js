05098242a93ef45abf90c6ea9a56b75905255bb2
nbunch2 <n.bunch5683@gmail.com>
2016-12-13 22:22:12 -0700

    much change

--- save/scripts/contact_info.js
+++ save/scripts/contact_info.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){
     $.getJSON( "/save/scripts/contact_info.json", function( data ) {
   console.log(data);
-    $(".info").text(data.TroyHicks.story);
+    $(".info").text(data.contact.story);
+        $(".videointro").text(data.contact.video);
+        $(".email").text(data.information.email);
     
     });
     
